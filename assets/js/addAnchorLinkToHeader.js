d88a51cb58cda81e96c2e8a3aca210f625aa0345
CBB <crystalballbe@gmail.com>
2020-11-04 09:34:50 +0900

    Add anchor links; Cut out script as js file

---
+++ assets/js/addAnchorLinkToHeader.js
@@ -0,0 +1,7 @@
+
+/*
+In order to add deep anchor links to existing page content, using "AnchorJS".
+https://github.com/bryanbraun/anchorjs
+*/
+anchors.options.placement = "left"
+anchors.add('h3:not(.no-anchor)');
